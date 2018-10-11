25b2f18b47a5fd88fec42540614a5bdec1b681c1
garyCoffey <garycoffey12@gmail.com>
2018-10-11 14:45:51 -0500

    Added funtionality to determine turn and almost finished with winner declaration

--- src/index.js
+++ src/index.js
@@ -3,37 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null
-		}
-	}
-	render() {
-		return (
-			<button
-				className="square"
-				onClick={() => this.props.onClick()}
-			>
-				{this.props.value}
-			</button>
-		);
-	}
+function Square(props) { 
+	return (
+		<button className="square" onClick={props.onClick}>
+		{props.value}
+		</button>
+	);
 }
+
 
 class Board extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			squares: Array(9).fill(null)
+			squares: Array(9).fill(null),
+			xIsNext: true
 		};
 	}
+
 	handleClick(i) {
 		const squares = this.state.squares.slice();
 		squares[i] = 'X';
-
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext
+		});
 	}
+
 	renderSquare(i) {
 		return (
 			<Square
@@ -44,7 +40,8 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = 'Next player: X';
+		const status = 'Next player: ' + (this.state.xIsNext ? 'X' :
+			'O');
 
 		return (
 			<div>
@@ -85,7 +82,29 @@ class Game extends React.Component {
 	}
 }
 
-//=========================================
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
+
+//=========================================c
 
 
 ReactDOM.render(
